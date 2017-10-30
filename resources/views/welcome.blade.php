4308acc213dda219601908044822cb571a0ffd95
DarknorthDaoist <ricardo.alvarezz@usach.cl>
2017-10-30 12:27:59 -0300

    Front
    Ahora si con bootstrap 4, no con w3.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 @section('content')
 
-<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
-  <img class="w3-image" src="/IMG/incendio.jpg" alt="incendio" width="1500" height="800">
-  <div class="w3-display-middle  w3-center">
-    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-blue w3-opacity-min"><b>Chile</b></span> <span class="w3-padding w3-red w3-opacity-min">Emergencias</span></h1>
-    <h1 class="w3-xxlarge w3-text-black"><span class="w3-padding w3-white w3-opacity-min"><b>Chile Apoya a Chile</b></span></h1>
-  </div>
-</header>
+<section class="container-fluid slider d-flex flex-column justify-content-center align-items-center sticky-top">
+	<div class="row ">
+		<h1 class="display-4 font-weight-bold" style="background-color: #0014B2;">Chile</h1><h1 class="display-4 bg-white font-weight-bold">Emergencias</h1>
+	</div>
+	<div class="row">
+		<h1 class="display-4" style="background-color: #E8300E;">Chile ayuda a Chile</h1>
+	</div>
+</section>
+
+
 @stop
    
 
