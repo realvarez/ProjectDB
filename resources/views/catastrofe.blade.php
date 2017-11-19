bc22ce50bfceb86475fe63f9d1aed03a44425f30
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2017-11-19 02:08:29 -0300

    avance vista de medidas para cada catastrofe

---
+++ resources/views/catastrofe.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+
+<section class="container">
+  <div class="row">
+    <div class="col-md-12">
+        aca ira la descripcion de la catastrofe
+    </div>
+
+    <div class="col-md-6">
+
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        <h3 class="panel-title">Panel 1</h3>
+        <span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-up"></i></span>
+      </div>
+      <div class="panel-body">Panel content</div>
+    </div>
+
+    </div>
+
+    <div class="col-md-6">
+      aca se deberian mostrar la descricion de cada medida al seleccionar alguna
+    </div>
+
+  </div>
+</section>
