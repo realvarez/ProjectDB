b857c642ba865dd6652e14ad174f1ace641ff393
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2017-11-21 13:46:07 -0300

    vista de medidas para cada catastrofe4

--- resources/views/catastrofe.blade.php
+++ resources/views/catastrofe.blade.php
@@ -1,16 +1,43 @@
 @extends('layouts.app')
 @section('content')
 
-<section class="container">
+<section class="container-fluid">
   <div class="row">
     <div class="col-md-12 mt-5 border-bottom-4 border-success rounded-bottom-3">
-        <h4>{{$c->titutulo}}</h4>
-        <p>{{$c->tipoC}}</p>
-        <p>{{$c->comuna->nombre}}</p>
-        <p>{{$c->descripcion}}</p>
+      <div class="row">
+        <div class="col-md-8">
+
+          <h4>{{$c->titutulo}}</h4>
+          <div class="Descr_c">
+            <h5>Descripcion</h5>
+            <p>{{$c->descripcion}}</p>
+          </div>
+          <div class="Especif">
+            <h5>Comuna:</h5>
+            <p>{{$c->comuna->nombre}}</p>
+            <h5>Categoria:</h5>
+            <p>{{$c->tipoC}}  </p>
+            <h5>Fecha de Inicio:</h5>
+            <p>------------------</p>
+          </div>
+        </div>
+        <div class="col-md-4 d-flex justify-content-center lign-items-center">
+
+          <div class="progress blue">
+                <span class="progress-left">
+                    <span class="progress-bar"></span>
+                </span>
+                <span class="progress-right">
+                    <span class="progress-bar"></span>
+                </span>
+                <div class="progress-value">90%</div>
+            </div>
+
+        </div>
+      </div>
     </div>
 
-    <div class="col-md-12">
+    <div class="col-md-12 px-5 py-3">
     @foreach ($medidas as $m)
     <div class="panel panel-primary">
       <div class="panel-heading">
