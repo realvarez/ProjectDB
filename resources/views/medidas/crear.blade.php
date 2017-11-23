049ed00d820afe3a44eaf0678f066028a9a31332
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2017-11-23 12:23:07 -0300

    formulario preliminar de las medidas

--- resources/views/medidas/crear.blade.php
+++ resources/views/medidas/crear.blade.php
@@ -18,8 +18,43 @@
               @endforeach
             </select>
           </div>
-        
-       
+
+          <script src="http://momentjs.com/downloads/moment-with-locales.js"></script>
+  <script src="http://momentjs.com/downloads/moment-timezone-with-data.js"></script>
+
+
+
+
+  <div class="container" style="margin-top:70px;">
+    <div class="row">
+          <form class="form-horizontal col-sm-7 col-sm-offset-2" action="" method="post">
+              <div class="form-group registration-date">
+                  <label class="control-label col-sm-3" for="registration-date">Fecha Inicio:</label>
+                <div class="input-group registration-date-time">
+                  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></span>
+                  <input class="form-control" name="registration_date" id="registration-date" type="date">
+                </div>
+              </div>
+          </form>
+    </div>
+  </div>
+
+  <div class="container" style="margin-top:70px;">
+    <div class="row">
+          <form class="form-horizontal col-sm-7 col-sm-offset-2" action="" method="post">
+              <div class="form-group registration-date">
+                  <label class="control-label col-sm-3" for="registration-date">Fecha Termino:</label>
+                <div class="input-group registration-date-time">
+                  <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></span>
+                  <input class="form-control" name="registration_date" id="registration-date" type="date">
+                </div>
+              </div>
+          </form>
+    </div>
+  </div>
+
+
+
 
             <a href="{{route('voluntarios.create')}}" class="btn-primary">Siguiente</a>
         </form>
