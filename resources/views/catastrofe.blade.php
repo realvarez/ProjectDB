f82f49faec7726ad77b589c1242e027dfc51179c
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2017-11-24 23:08:17 -0300

    casi terminada vista medidas

--- resources/views/catastrofe.blade.php
+++ resources/views/catastrofe.blade.php
@@ -51,10 +51,18 @@
         <div class=" col-md-6 w-50">
 
 
-              <div class="card  border border-primary" >
+              <div class="card   border border-primary" >
                 <div class="card-body">
                   <h4 class="card-title text-uppercase border border-left-0 border-right-0 border-top-0 border-succses"> {{$m->titulo}} </h4>
-                  <h6 class="card-subtitle mb-2 text-muted">tipo</h6>
+                  @if($m->MorphMedida_type=="App\Evento")
+                  <h6 class="card-subtitle mb-2 text-success">Evento a beneficio</h6>
+                  @elseif($m->MorphMedida_type=="App\Apoyo_economico")
+                  <h6 class="card-subtitle mb-2 text-danger">Apoyo Economico</h6>
+                  @elseif($m->MorphMedida_type=="App\Voluntariado")
+                  <h6 class="card-subtitle mb-2 text-info">Solicitud de Voluntarios</h6>
+                  @else
+                  <h6 class="card-subtitle mb-2 text-secondary">Recoleccion Centro de Acopio</h6>
+                  @endif
                   <!-- <p class="card-text">{{$m->descripcion}}</p> -->
                   <div class="d-flex justify-content-between">
 
