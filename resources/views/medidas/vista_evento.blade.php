100d37d6c7f0e5bf5ea63f90c774670b0882e2e8
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2017-11-27 23:16:23 -0300

    email

--- resources/views/medidas/vista_evento.blade.php
+++ resources/views/medidas/vista_evento.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
-<section class="container-fluid border border-primary p-3 mb-2 bg-light text-dark w-50">
-  <div class="row">
+<section class="container-fluid ">
+  <div class="row border border-primary p-3 mb-2 bg-light text-dark w-50">
     <div class="col-md-12">
       <h3>Evento a Beneficio</h3>
     </div>
@@ -29,6 +29,36 @@
     <a class="btn btn-primary" href="{{route('participa.create',$evento->medida->id)}}"> Participar</a>
   </div>
 
+  <div class="row d-flex justify-content-center">
+
+    <div class="col-md-8 bg-light border border-light">
+        <h4>Comentarios</h4>
+        <div class="">
+            <div class="card">
+              <div class="card-header">
+                Nombre Usuario
+              </div>
+              <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                  <p>comentario</p>
+                  <p>fecha de publicacion</p>
+                </blockquote>
+              </div>
+            </div>
+        </div>
+        <div class="mt-3">
+          <form class="" action="index.html" method="post">
+            <div class="form-group">
+              <label for="comentario">Ingresa tu Comentario</label>
+              <textarea class="form-control" id="comentario" name="comentario" rows="3"></textarea>
+            </div>
+            <button type="submit" id="submit" name="submit" class="btn btn-success">Comentar</button>
+          </form>
+
+        </div>
+    </div>
+
+  </div>
 
 
 </section>
